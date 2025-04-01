1502319b5893913c505736ba881c203108da2427
SRIKAR 9080 <159053813+tacaz676@users.noreply.github.com>
2025-04-01 21:36:53 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Medical-based-ai-bot-
+
+
+
+Clone the repository 
+
+''' bash
+Project repo: https://github.com/
+
+''' bash
+conda create -n medibot python=3.10 -y
+
+'''bash
+conda activate medibot
+
+''' bash
+pip install -r requirement.txt
---
+++ template.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s")
+
+# List of files to create
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "requirement.txt",
+    "setup.py",
+    "app.py",
+    "research/trails.ipynb"
+]
+
+# Iterate over the files and create them if needed
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    # Create directory if it doesn't exist
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    # Create file if it doesn't exist or is empty
+    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
+        with open(filepath, "w") as f:
+            pass  # Create an empty file
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
